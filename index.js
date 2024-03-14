02b0e94c8439e184101d3b0f974e6ddd714e4ddb
Lucio Callegare <luciocallegare@gmail.com>
2024-03-14 01:39:20 -0300

    add corse enabled

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const files = require('./routes/files')
+const cors = require('cors')
 
 app.listen(port, () => console.log(`App running on port ${3000}!`))
+app.use(cors())
+
+
+app.use((err,req,res,next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 
 app.get('/health', (req, res) => {
   res.send('App running succesfully')
