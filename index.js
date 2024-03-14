832080c548cfcaaf114286bd6ce9489086a0fe4a
Lucio Callegare <luciocallegare@gmail.com>
2024-03-14 01:44:32 -0300

    delete useless midleware

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const cors = require('cors')
 app.listen(port, () => console.log(`App running on port ${3000}!`))
 app.use(cors())
 
-
-app.use((err,req,res,next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
-
 app.get('/health', (req, res) => {
   res.send('App running succesfully')
 })
